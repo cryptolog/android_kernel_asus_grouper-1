d6a1d612ff64a5291a7c643bc72315e4a6a83f0e
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2012-03-13 19:18:39 -0400

    provide disable_cpufreq() function to disable the API.
    useful for disabling cpufreq altogether. The cpu frequency
    scaling drivers and cpu frequency governors will fail to register.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Signed-off-by: Dave Jones <davej@redhat.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2026,6 +2026,9 @@ int cpufreq_register_driver(struct cpufreq_driver *driver_data)
 	if (cpufreq_disabled())
 		return -ENODEV;
 
+	if (cpufreq_disabled())
+		return -ENODEV;
+
 	if (!driver_data || !driver_data->verify || !driver_data->init ||
 	    ((!driver_data->setpolicy) && (!driver_data->target)))
 		return -EINVAL;
@@ -2152,6 +2155,9 @@ static int __init cpufreq_core_init(void)
 {
 	int cpu;
 	int rc;
+
+	if (cpufreq_disabled())
+		return -ENODEV;
 
 	if (cpufreq_disabled())
 		return -ENODEV;
