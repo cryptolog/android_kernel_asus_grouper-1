CyanogenMod/android_kernel_asus_grouper

bq27541: Add current_now property
The current is returned by the charger as a twos
complement signed integer in milliamps.  Applications
such as CurrentWidget expect this to be microamps.

Signed-off-by: Andrew Dodd <atd7@cornell.edu>
Change-Id: I053b3d1f24ec04391e982fdcb4b3486241c3aa34

drivers/power/bq27541_battery.c
@@ -140,6 +140,7 @@ static enum power_supply_property bq27541_properties[] = {
 	POWER_SUPPLY_PROP_PRESENT,
 	POWER_SUPPLY_PROP_TECHNOLOGY,
 	POWER_SUPPLY_PROP_VOLTAGE_NOW,
+	POWER_SUPPLY_PROP_CURRENT_NOW,
 	POWER_SUPPLY_PROP_CAPACITY,
 	POWER_SUPPLY_PROP_TEMP,
 };
@@ -481,6 +482,18 @@ static int bq27541_get_psp(int reg_offset, enum power_supply_property psp,
 	if (psp == POWER_SUPPLY_PROP_VOLTAGE_NOW) {
 		val->intval = bq27541_device->bat_vol = rt_value;
 		BAT_NOTICE("voltage_now= %u mV\n", val->intval);
+	}
+	if (psp == POWER_SUPPLY_PROP_CURRENT_NOW) {
+		val->intval = rt_value;
+		/* Returns a signed 16-bit value in mA */
+		if (val->intval & 0x8000) {
+			/* Negative */
+			val->intval = ~val->intval & 0x7fff;
+			val->intval++;
+			val->intval *= -1;
+		}
+		val->intval *= 1000;
+		BAT_NOTICE("current_now= %u uA\n", val->intval);
 	}
 	if (psp == POWER_SUPPLY_PROP_STATUS) {
 		ret = bq27541_device->bat_status = rt_value;

cryptolog/android_kernel_asus_grouper-1

drivers/power/bq27541_battery.c
@@ -147,6 +147,7 @@ static enum power_supply_property bq27541_properties[] = {
 	POWER_SUPPLY_PROP_PRESENT,
 	POWER_SUPPLY_PROP_TECHNOLOGY,
 	POWER_SUPPLY_PROP_VOLTAGE_NOW,
+	POWER_SUPPLY_PROP_CURRENT_NOW,
 	POWER_SUPPLY_PROP_CAPACITY,
 	POWER_SUPPLY_PROP_TEMP,
 	POWER_SUPPLY_PROP_CURRENT_NOW,
@@ -545,6 +546,18 @@ static int bq27541_get_psp(int reg_offset, enum power_supply_property psp,
 			val->intval = bq27541_device->bat_vol;
 		}
 		dev_dbg(&bq27541_device->client->dev, "voltage_now=%u\n", val->intval);
+	}
+	if (psp == POWER_SUPPLY_PROP_CURRENT_NOW) {
+		val->intval = rt_value;
+		/* Returns a signed 16-bit value in mA */
+		if (val->intval & 0x8000) {
+			/* Negative */
+			val->intval = ~val->intval & 0x7fff;
+			val->intval++;
+			val->intval *= -1;
+		}
+		val->intval *= 1000;
+		BAT_NOTICE("current_now= %d uA\n", val->intval);
 	}
 	if (psp == POWER_SUPPLY_PROP_STATUS) {
 		ret = bq27541_device->bat_status = rt_value;