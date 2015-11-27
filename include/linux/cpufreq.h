da2c53d8ad0b85f13df4ba37dd30b70f57329755
coderzstas <coderz35@gmail.com>
2015-11-27 17:13:04 +0300

    Revert "umbrella_core: initial version for Tegra devices"
    This reverts commit 85969942d54c45ee0aaf41cbe4fd0f81199f1897.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -391,9 +391,6 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
 extern struct cpufreq_governor cpufreq_gov_yankactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_UMBRELLA_CORE)
-extern struct cpufreq_governor cpufreq_gov_umbrella_core;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_umbrella_core)
 #endif
 
 
