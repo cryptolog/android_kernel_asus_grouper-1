1a6755b49d4e2595e7e7ebd6b86939e1dfffbd66
coderzstas <coderz35@gmail.com>
2015-11-28 15:50:35 +0300

    Revert "move security_mmap_addr() to saner place"
    This reverts commit abd0ccb97ac0150505dde427260b141dd55a291f.

--- mm/mmap.c
+++ mm/mmap.c
@@ -996,6 +996,7 @@ static unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 	struct mm_struct * mm = current->mm;
 	struct inode *inode;
 	vm_flags_t vm_flags;
+	int error;
 
 	/*
 	 * Does the application expect PROT_READ to imply PROT_EXEC?
@@ -1116,6 +1117,10 @@ static unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 			return -EINVAL;
 		}
 	}
+
+	error = security_mmap_addr(addr);
+	if (error)
+		return error;
 
 	return mmap_region(file, addr, len, flags, vm_flags, pgoff);
 }
@@ -1660,9 +1665,7 @@ get_unmapped_area(struct file *file, unsigned long addr, unsigned long len,
 	if (addr & ~PAGE_MASK)
 		return -EINVAL;
 
-	addr = arch_rebalance_pgtables(addr, len);
-	error = security_mmap_addr(addr);
-	return error ? error : addr;
+	return arch_rebalance_pgtables(addr, len);
 }
 
 EXPORT_SYMBOL(get_unmapped_area);
@@ -2265,6 +2268,10 @@ static unsigned long do_brk(unsigned long addr, unsigned long len)
 	if (!len)
 		return addr;
 
+	error = security_mmap_addr(addr);
+	if (error)
+		return error;
+
 	flags = VM_DATA_DEFAULT_FLAGS | VM_ACCOUNT | mm->def_flags;
 
 	error = get_unmapped_area(NULL, addr, len, 0, MAP_FIXED);
@@ -2608,6 +2615,10 @@ int install_special_mapping(struct mm_struct *mm,
 
 	vma->vm_ops = &special_mapping_vmops;
 	vma->vm_private_data = pages;
+
+	ret = security_mmap_addr(vma->vm_start);
+	if (ret)
+		goto out;
 
 	ret = insert_vm_struct(mm, vma);
 	if (ret)
